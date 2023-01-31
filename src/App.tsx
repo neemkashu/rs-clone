d0d61db45e17ab292bf18e3b23ca1e9103d99e76
neemkashu <sparjinkust@mail.ru>
2023-01-31 16:06:26 +0300

    chore: add configs files for react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.scss';
+
+function App(): JSX.Element {
+    return (
+        <>
+            <div className="App">
+                <header className="App-header">Hello!</header>
+            </div>
+            <div className="App">
+                <header className="App-header">Hello!</header>
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const rootDiv = document.getElementById('root');
+if (!rootDiv) {
+  throw new Error('root is not found!');
+}
+const root = ReactDOM.createRoot(rootDiv);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
