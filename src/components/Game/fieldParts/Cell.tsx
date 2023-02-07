6ee0ddbee7850745dae0a677c65ea0f1bb606520
neemkashu <sparjinkust@mail.ru>
2023-02-07 16:36:07 +0300

    fix: fix styles for cells

--- src/components/Game/fieldParts/Cell.tsx
+++ src/components/Game/fieldParts/Cell.tsx
@@ -3,7 +3,9 @@ import { CellProps } from '../../../utils/types';
 export default function Cell({ cellContent, styles }: CellProps): JSX.Element {
     return (
         <td className="cell-square">
-            <div className={`square ${styles?.join(' ')}`}>{cellContent ?? ''}</div>
+            <div className={`square lh-1 text-center ${styles?.join(' ')}`}>
+                {cellContent ?? ''}
+            </div>
         </td>
     );
 }
