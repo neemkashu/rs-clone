6eaf8cd8607fc5ec176a59544a192a2501a021c9
neemkashu <sparjinkust@mail.ru>
2023-02-09 18:59:02 +0300

    feat: chronometer watches first click or non-zero user time

--- src/components/Game/Chronometer.tsx
+++ src/components/Game/Chronometer.tsx
@@ -1,9 +1,14 @@
 import { useCallback, useContext, useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { userNonogramData } from '../../utils/mochas'; // imitation of back-end info
 import { getTimeFromStorage, setTimeToStorage } from '../../utils/helpers';
 import './gameStyles/Chronometer.scss';
 import { NonogramRaw } from '../../utils/types';
 import { GameStatusContext } from './contexts/context';
+import { useAppSelector } from '../hooks';
+import { store } from '../store';
+import { GameStatus } from './gameUtils/types';
+import { firstFieldClick } from './gameSlice';
 
 const REFRESH_PERIOD = 1000;
 
@@ -18,6 +23,15 @@ const isUserLogged = () => false;
 
 function Chronometer({ nonogramRaw }: { nonogramRaw: NonogramRaw | null }): JSX.Element {
     const nonogramID = nonogramRaw?.id;
+    const dispatch = useDispatch();
+    const gameState = useAppSelector((state) => state.game.status);
+
+    const shouldUpdateStatus =
+        getTimeFromStorage(nonogramID) > 0 &&
+        (gameState === GameStatus.INITIAL || gameState === null);
+    if (shouldUpdateStatus) {
+        dispatch(firstFieldClick(GameStatus.STARTED));
+    }
 
     const [userTime, setUserTime] = useState(
         isUserLogged()
@@ -32,17 +46,17 @@ function Chronometer({ nonogramRaw }: { nonogramRaw: NonogramRaw | null }): JSX.
             setUserTime(currentTimer);
             setTimeToStorage(currentTimer, nonogramID);
         };
-
+        const isGameRunning =
+            gameState === GameStatus.INITIAL || gameState === GameStatus.STARTED;
         const timer = setInterval(timeStoreAndRefresh, REFRESH_PERIOD);
-
-        if (isPageHidden) {
+        if (isPageHidden || !isGameRunning) {
             clearInterval(timer);
         }
 
         return () => {
             clearInterval(timer);
         };
-    }, [userTime, isPageHidden, nonogramID]);
+    }, [userTime, isPageHidden, nonogramID, gameState]);
 
     document.onvisibilitychange = (event) => {
         setIsPageHidden(document.hidden);
