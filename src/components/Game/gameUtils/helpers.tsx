5b55b33bc71900aae77893e3400829afdd79c2d0
neemkashu <sparjinkust@mail.ru>
2023-02-09 14:29:44 +0300

    feat: add redux game slice

---
+++ src/components/Game/gameUtils/helpers.tsx
@@ -0,0 +1,73 @@
+import { StorageKeys } from './storage';
+import { NonogramTime } from './types';
+
+export function getUserCurrentTimes(
+    currentTimesInfo: string | null
+): NonogramTime[] | null {
+    if (!currentTimesInfo) {
+        return null;
+    }
+    const parsedInfo = JSON.parse(currentTimesInfo);
+
+    const isCorrectData =
+        Array.isArray(parsedInfo) &&
+        Object.hasOwn(parsedInfo[0], 'id') &&
+        Object.hasOwn(parsedInfo[0], 'time');
+    if (isCorrectData) {
+        return parsedInfo as NonogramTime[];
+    }
+    return null;
+}
+export function setTimeToStorage(time: number, id?: string): void {
+    if (!id) {
+        return;
+    }
+    const storageInfo = localStorage.getItem(StorageKeys.USER_CURRENT_TIME);
+    const storageTimes = getUserCurrentTimes(storageInfo);
+
+    if (Array.isArray(storageTimes)) {
+        const nonogramIndex = storageTimes.findIndex(
+            (nonogramInfo) => nonogramInfo.id === id
+        );
+        if (nonogramIndex > -1) {
+            storageTimes[nonogramIndex] = { id, time };
+        } else {
+            storageTimes.push({ id, time });
+        }
+        localStorage.setItem(StorageKeys.USER_CURRENT_TIME, JSON.stringify(storageTimes));
+    } else {
+        localStorage.setItem(
+            StorageKeys.USER_CURRENT_TIME,
+            JSON.stringify([{ id, time }])
+        );
+    }
+}
+export function getTimeFromStorage(id?: string): number {
+    if (!id) {
+        return 0;
+    }
+    const storageInfo = localStorage.getItem(StorageKeys.USER_CURRENT_TIME);
+    const storageTimes = getUserCurrentTimes(storageInfo);
+    if (storageTimes !== null) {
+        const currentTimebyID = storageTimes.find((nonogramInfo) => {
+            return nonogramInfo.id === id;
+        });
+        return currentTimebyID?.time ?? 0;
+    }
+    return 0;
+}
+export function unifyTwoDimensionalArray<T>(arr?: T[][]): (T | null)[][] {
+    if (!arr) {
+        return [[null]];
+    }
+    const innerArrMaxLength = arr.reduce((maxLength, innerArr) => {
+        return maxLength > innerArr.length ? maxLength : innerArr.length;
+    }, 1);
+    const arrUnified: (T | null)[][] = [...arr];
+    arrUnified.forEach((column) => {
+        while (column.length < innerArrMaxLength) {
+            column.unshift(null);
+        }
+    });
+    return arrUnified;
+}
