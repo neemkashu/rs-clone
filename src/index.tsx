13348cab139d2407f25bea66ef65eb60ebe0a32a
Vlad_nikishin <Nik_vladislav@outlook.com>
2023-02-09 15:51:03 +0300

    refactor: test after rebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import { store } from './components/store';
