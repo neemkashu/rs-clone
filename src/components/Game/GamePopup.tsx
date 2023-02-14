2757f883d86fac0cc3ca9d63f5cb4857c6fe4687
neemkashu <sparjinkust@mail.ru>
2023-02-14 13:14:35 +0300

    refactor: change modal visibility via boolean state

--- src/components/Game/GamePopup.tsx
+++ src/components/Game/GamePopup.tsx
@@ -13,42 +13,47 @@ export function GamePopup({
     captions: ModalCaptions;
     children: JSX.Element[];
 }): JSX.Element {
-    const modalStates = ['block', 'none'];
-    const [modalDisplay, setmodalDisplay] = useState(modalStates[0]);
+    const [isVisible, setIsVisible] = useState(true);
     const modalHandler = () => {
-        const modalStyle =
-            modalDisplay === modalStates[0] ? modalStates[1] : modalStates[0];
-        setmodalDisplay(modalStyle);
+        setIsVisible((prevoius) => !prevoius);
     };
     return (
-        <div
-            className="modal fade show"
-            tabIndex={-1}
-            style={{ display: modalDisplay, backgroundColor: 'rgba(130,130,130,0.5)' }}
-            onClick={modalHandler}
-            role="presentation"
-        >
-            <div
-                className="modal-dialog"
-                onClick={(event) => {
-                    event?.stopPropagation();
-                }}
-                role="presentation"
-            >
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <div className="modal-title">{captions.modalTitle}</div>
-                        <button
-                            type="button"
-                            onClick={modalHandler}
-                            className="btn-close"
-                            aria-label="Close"
-                        />
+        <>
+            <div />
+            {isVisible && (
+                <div
+                    className="modal fade show"
+                    tabIndex={-1}
+                    style={{
+                        display: 'block',
+                        backgroundColor: 'rgba(130,130,130,0.5)',
+                    }}
+                    onClick={modalHandler}
+                    role="presentation"
+                >
+                    <div
+                        className="modal-dialog"
+                        onClick={(event) => {
+                            event?.stopPropagation();
+                        }}
+                        role="presentation"
+                    >
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <div className="modal-title">{captions.modalTitle}</div>
+                                <button
+                                    type="button"
+                                    onClick={modalHandler}
+                                    className="btn-close"
+                                    aria-label="Close"
+                                />
+                            </div>
+                            <div className="modal-body">{children[0]}</div>
+                            <div className="modal-footer">{children[1]}</div>
+                        </div>
                     </div>
-                    <div className="modal-body">{children[0]}</div>
-                    <div className="modal-footer">{children[1]}</div>
                 </div>
-            </div>
-        </div>
+            )}
+        </>
     );
 }
