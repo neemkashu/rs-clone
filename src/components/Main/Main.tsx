faf3008867fa0f85ae77d971189a6b3c0f746b45
neemkashu <sparjinkust@mail.ru>
2023-02-23 17:23:29 +0300

    feat: add main page content

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,3 +1,14 @@
+import { useTranslation } from 'react-i18next';
+
+/* eslint-disable react/no-unescaped-entities */
 export function Main(): JSX.Element {
-    return <div>Main Page</div>;
+    const { t } = useTranslation();
+    return (
+        <div>
+            <h1>{t('mainHeader')}</h1>
+            <p>{t('mainGreeting')}</p>
+            <p>{t('mainAbout')}</p>
+            <p>{t('mainAppeal')}</p>
+        </div>
+    );
 }
