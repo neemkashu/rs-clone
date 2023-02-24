9411a93f536a2f28b6e64097a9b5f8edf1351577
Anna Perevedentseva <anna.pereved@gmail.com>
2023-02-24 01:43:30 +0100

    feat: add animation

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,39 @@
+import './Main.scss';
 import { useTranslation } from 'react-i18next';
 
 /* eslint-disable react/no-unescaped-entities */
 export function Main(): JSX.Element {
     const { t } = useTranslation();
     return (
-        <div>
+        <div className="position-relative">
+            <div className="sakura">
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+            </div>
+
             <h1>{t('mainHeader')}</h1>
             <p>{t('mainGreeting')}</p>
             <p>{t('mainAbout')}</p>
